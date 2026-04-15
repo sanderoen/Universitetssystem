0909f1fc81a36fa2235d944662ec2bf1a21649f1
sanderoen <sanderoen@outlook.com>
2026-04-15 13:48:38 +0200

    Update Utvekslingsstudent.cs

--- Utvekslingsstudent.cs
+++ Utvekslingsstudent.cs
@@ -1,1 +1,15 @@
+namespace Universitets_system_oppgave_2;
+
+public class Utvekslingstudent : Student
+{
+    public string Hjemuniversitet { get; set; }
+    public string Land { get; set; }
+    public string fra { get; set; }
+    public string til { get; set; }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" {Hjemuniversitet} {Land}";
+    }
+}
 
