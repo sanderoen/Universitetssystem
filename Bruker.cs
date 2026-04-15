bfce758e8a174ba1723a56432d10d07029b07cae
sanderoen <sanderoen@outlook.com>
2026-04-15 13:42:28 +0200

    Update Bruker.cs

--- Bruker.cs
+++ Bruker.cs
@@ -1,1 +1,17 @@
+namespace Universitets_system_oppgave_2;
 
+
+public class Bruker
+{
+    public string Navn { get; set; }
+    public string Epost { get; set; }
+    public string Brukernavn { get; set; }
+    public string Passord { get; set; }
+    public string Rolle { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Navn} {Epost} {Rolle}";
+    }
+}
+        
