2f3bf14a5a5146f850129698fcdf92b16c808f33
sanderoen <sanderoen@outlook.com>
2026-04-15 13:45:07 +0200

    Update Bok.cs

--- Bok.cs
+++ Bok.cs
@@ -1,1 +1,16 @@
+namespace Universitets_system_oppgave_2;
 
+
+public class Bok
+{
+    public string ID { get; set; }
+    public string tittel { get; set; }
+    public string Forfatter { get; set; }
+    public int år { get; set; }
+    public int antalleksemplar { get; set; }
+
+    public override string ToString()
+    {
+        return $"{ID} {tittel} {Forfatter} {år} Antall:{antalleksemplar}";
+    }
+}
