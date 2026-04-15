a44a5b6c2748d9100e09092f17269667800bf614
sanderoen <sanderoen@outlook.com>
2026-04-15 13:53:07 +0200

    Create Lån.cs

---
+++ Lån.cs
@@ -0,0 +1,17 @@
+namespace Universitets_system_oppgave_2;
+
+
+public class Lån
+{
+    public string BokID { get; set; }
+    public string BrukerID { get; set; }
+    public bool Aktiv { get; set; } = true;
+    public DateTime Låndato { get; set; } = DateTime.Now;
+    public DateTime? Returdato { get; set; }
+
+    public override string ToString()
+    {
+        string status = Aktiv ? "AKTIV" : "RETURNERT";
+        return $"{status} Bok:{BokID} Bruker:{BrukerID} Lånt:{Låndato} Returnert:{Returdato}";
+    }
+}
