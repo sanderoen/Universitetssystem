1d2f2558f906b8f321ba80e2bc89189eccb74885
sanderoen <sanderoen@outlook.com>
2026-04-15 13:45:45 +0200

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,1 +1,13 @@
+sing Universitets_system_oppgave_2;
 
+
+public class Student : Bruker
+{
+    public string StudentID { get; set; }
+    public List<string> kurskoder { get; set; } = new List<string>();
+
+    public override string ToString()
+    {
+        return $"{StudentID} {Navn} {Epost}";
+    }
+}
